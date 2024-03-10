254c8cd8045c2c49a7a2b74157f7f04ad737673c
MiniProject-akp <miniprojectakp@gmail.com>
2024-03-10 09:54:47 +0530

    feedback page done

---
+++ src/feedback.py
@@ -0,0 +1,68 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+class Feedback:
+
+    def __init__(self, master):
+
+        master.title('SCRAPY-X  USER  REVIEW')
+        master.resizable(False, False)
+        master.configure(background='#000000')  # Set background color to black
+
+        self.style = ttk.Style()
+        self.style.configure('TFrame', background='#000000')
+        self.style.configure('TButton', background='#000000', foreground='white', font=('Barlow', 11))  # Set default button style
+        self.style.configure('TLabel', background='#000000', font=('Barlow', 11, 'bold'), foreground='white')
+        self.style.configure('header.TLabel', font=('Barlow', 18, 'bold'), foreground='white')
+
+        self.frame_header = ttk.Frame(master)  # header frame
+        self.frame_header.pack()
+
+        ttk.Label(self.frame_header, text="Thanks for Exploring!", style='header.TLabel').grid(row=0, column=0)
+        ttk.Label(self.frame_header, text="We're glad you chose to explore SCRAPY-X!\nPlease tell us how it was.",
+                  justify='center').grid(row=1, column=0)
+
+        self.frame_content = ttk.Frame(master)  # content frame
+        self.frame_content.pack(pady=10)  # Add padding to the frame
+
+        ttk.Label(self.frame_content, text='Name:').grid(row=0, column=0, padx=5, pady=5, sticky='sw')  # Add pady to shift downwards
+        ttk.Label(self.frame_content, text='Email:').grid(row=0, column=1, padx=5, pady=5, sticky='sw')  # Add pady to shift downwards
+        ttk.Label(self.frame_content, text='Comments:').grid(row=2, column=0, padx=5, pady=5, sticky='sw')  # Add pady to shift downwards
+
+        self.entry_name = ttk.Entry(self.frame_content, width=24, font=('Barlow', 10))
+        self.entry_email = ttk.Entry(self.frame_content, width=24, font=('Barlow', 10))
+        self.text_comments = Text(self.frame_content, width=50, height=5, font=('Barlow', 10), foreground='white')  # Adjust height of the text box
+
+        self.entry_name.grid(row=1, column=0, padx=5, pady=5)  # Add pady to shift downwards
+        self.entry_email.grid(row=1, column=1, padx=5, pady=5)  # Add pady to shift downwards
+        self.text_comments.grid(row=3, column=0, columnspan=2, padx=5, pady=5)  # Add pady to shift downwards
+
+        ttk.Button(self.frame_content, text='Submit', command=self.submit, style='SubmitButton.TButton').grid(row=4, column=0, padx=5, pady=5, sticky='e')  # Add pady
+        ttk.Button(self.frame_content, text='Clear', command=self.clear, style='ClearButton.TButton').grid(row=4, column=1, padx=5, pady=5, sticky='w')  # Add pady
+
+        # Custom style for Submit and Clear buttons
+        self.style.configure('SubmitButton.TButton', foreground='black', font=('Barlow', 11, 'bold'))
+        self.style.configure('ClearButton.TButton', foreground='black', font=('Barlow', 11, 'bold'))
+
+    def submit(self):
+        print('Name: {}'.format(self.entry_name.get()))
+        print('Email: {}'.format(self.entry_email.get()))
+        print('Comments: {}'.format(self.text_comments.get(1.0, 'end')))
+        self.clear()
+        messagebox.showinfo(title="SCRAPY-X User Review", message="Comments Submitted!")
+
+    def clear(self):
+        self.entry_name.delete(0, 'end')
+        self.entry_email.delete(0, 'end')
+        self.text_comments.delete(1.0, 'end')
+
+
+def main():
+    root = Tk()
+    feedback = Feedback(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
--- src/register.py
+++ src/register.py
@@ -1,35 +1,19 @@
-# Python program to create a basic form 
-# GUI application using the customtkinter module
+
 import customtkinter as ctk
 import tkinter as tk
 
-# Sets the appearance of the window
-# Supported modes : Light, Dark, System
-# "System" sets the appearance mode to 
-# the appearance mode of the system
 ctk.set_appearance_mode("dark") 
-
-# Sets the color of the widgets in the window
-# Supported themes : green, dark-blue, blue 
 ctk.set_default_color_theme("blue") 
-
-# Dimensions of the window
 appWidth, appHeight = 600, 700
 
-
-# App Class
 class App(ctk.CTk):
-    # The layout of the window will be written
-    # in the init function itself
+   
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # Sets the title of the window to "App"
+  
         self.title("Registration Page") 
-        # Sets the dimensions of the window to 600x700
         self.geometry(f"{appWidth}x{appHeight}") 
 
-        # Create Account Label
         self.createAccountLabel = ctk.CTkLabel(self,
                                 text="Create Account",
                                 font=("Arial", 16, "bold"),
@@ -39,75 +23,64 @@ class App(ctk.CTk):
                             padx=20, pady=20,
                             sticky="ew")
 
-        # Name Label
         self.nameLabel = ctk.CTkLabel(self,
                                 text="Name")
         self.nameLabel.grid(row=1, column=0,
                             padx=20, pady=10,
                             sticky="ew")
 
-        # Name Entry Field
         self.nameEntry = ctk.CTkEntry(self,
                         placeholder_text="John Doe")
         self.nameEntry.grid(row=1, column=1,
                             columnspan=3, padx=20,
                             pady=10, sticky="ew")
 
-        # Email Label
         self.emailLabel = ctk.CTkLabel(self,
                                 text="Email")
         self.emailLabel.grid(row=2, column=0,
                             padx=20, pady=10,
                             sticky="ew")
 
-        # Email Entry Field
         self.emailEntry = ctk.CTkEntry(self,
                         placeholder_text="example@example.com")
         self.emailEntry.grid(row=2, column=1,
                             columnspan=3, padx=20,
                             pady=10, sticky="ew")
 
-        # Create Password Label
         self.createPasswordLabel = ctk.CTkLabel(self, text="Create Password")
         self.createPasswordLabel.grid(row=3, column=0,
                         padx=20, pady=10,
                         sticky="ew")
 
-        # Create Password Entry Field
         self.createPasswordEntry = ctk.CTkEntry(self,
                             placeholder_text="Password", show="*")
         self.createPasswordEntry.grid(row=3, column=1,
                         columnspan=3, padx=20,
                         pady=10, sticky="ew")
 
-        # Confirm Password Label
         self.confirmPasswordLabel = ctk.CTkLabel(self, text="Confirm Password")
         self.confirmPasswordLabel.grid(row=4, column=0,
                         padx=20, pady=10,
                         sticky="ew")
 
-        # Confirm Password Entry Field
         self.confirmPasswordEntry = ctk.CTkEntry(self,
                             placeholder_text="Confirm Password", show="*")
         self.confirmPasswordEntry.grid(row=4, column=1,
                         columnspan=3, padx=20,
                         pady=10, sticky="ew")
 
-        # Occupation Label
         self.occupationLabel = ctk.CTkLabel(self,
                                     text="Occupation")
         self.occupationLabel.grid(row=5, column=0,
                                 padx=20, pady=10,
                                 sticky="ew")
 
-        # Occupation combo box
         self.occupationOptionMenu = ctk.CTkOptionMenu(self,
                                     values=["Student", "Professional"])
         self.occupationOptionMenu.grid(row=5, column=1,
                                     columnspan=3, padx=20,
                                     pady=10, sticky="ew")
 
-        # Register Button
         self.registerButton = ctk.CTkButton(self,
                                         text="Register",
                                         command=self.register)
@@ -115,21 +88,14 @@ class App(ctk.CTk):
                                         columnspan=2, padx=20, 
                                         pady=20, sticky="ew")
 
-    # This function is used to insert the 
-    # details entered by users into the textbox
     def register(self):
         text = self.createText()
         print(text)
-
-    # This function is used to get the selected 
-    # options and text from the available entry
-    # fields and boxes and then generates 
-    # a prompt using them
+    
     def createText(self):
         text = f"Name: {self.nameEntry.get()} \nEmail: {self.emailEntry.get()} \nOccupation: {self.occupationOptionMenu.get()}"
         return text
 
 if __name__ == "__main__":
     app = App()
-    # Used to run the application
     app.mainloop()	 
